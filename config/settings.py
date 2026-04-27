d692ee43de662fb036f79dbaac137520a4d9e3d9
RomashkaHH <Srom.cuzn@yandex.ru>
2026-04-27 21:27:00 +0500

    write readme

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = "django-insecure-study-task-tracker-key"
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
